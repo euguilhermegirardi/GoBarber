f3e1ef50e36390534f9dfaa3dafa7be5fcd6ed7c
Guilherme Girardi <girardi.gui@icloud.com>
2020-09-03 08:51:41 +0100

    Create token

---
+++ back-end/src/services/AuthenticateUserService.ts
@@ -0,0 +1,49 @@
+import { getRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import User from "../models/User";
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Incorrect email/password combination.");
+    }
+
+    // user.password = Cryptic password
+    // password = No Cryptic password
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error("Incorrect email/password combination.");
+    }
+
+    // md5 online for the 'hash'
+    const token = sign({}, "4f5d1e03ea3b699639ead1e6a3cde413", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
