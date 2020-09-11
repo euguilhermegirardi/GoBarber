8995ea7579860eb949a270d8a71691c62e9bced1
Guilherme Girardi <girardi.gui@icloud.com>
2020-09-11 08:22:33 +0100

    Edit input, Yup added to handle validation

--- front-end/src/components/Input/index.tsx
+++ front-end/src/components/Input/index.tsx
@@ -1,17 +1,69 @@
-import React, { InputHTMLAttributes } from "react";
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from "react";
 import { IconBaseProps } from "react-icons";
+import { useField } from "@unform/core";
+import useDidMount from "@rooks/use-did-mount";
 
 import { Container } from "./styles";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
+  const handleInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+    // if (inputRef.current?.value) {
+    //   setIsField(true);
+    // } else {
+    //   setIsField(false);
+    // }
+  }, []);
+
+  useDidMount(() => {
+    registerField({
+      ref: inputRef?.current,
+      path: "value",
+      name: fieldName,
+    });
+  });
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        ref={inputRef}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        {...rest}
+      />
+    </Container>
+  );
+};
 export default Input;
--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,40 +1,62 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
+import { Form } from "@unform/web";
+import * as Yup from "yup";
 
 import logoImg from "../../assets/logo.svg";
 import { Container, Content, Background } from "./styles";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Name is required"),
+        email: Yup.string()
+          .required("Email is required")
+          .email("Type a valid email"),
+        password: Yup.string().min(6, "At least 6 characters"),
+      });
 
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-      <form>
-        <h1>Login</h1>
+  return (
+    <Container>
+      <Background />
 
-        <Input name="name" icon={FiUser} type="text" placeholder="Name" />
-        <Input name="email" icon={FiMail} type="text" placeholder="Email" />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
+        <Form onSubmit={handleSubmit}>
+          <h1>Login</h1>
 
-        <Button type="submit">Register</Button>
-      </form>
+          <Input name="name" icon={FiUser} type="text" placeholder="Name" />
+          <Input name="email" icon={FiMail} type="text" placeholder="Email" />
 
-      <a href="create">
-        <FiArrowLeft />
-        Back to Login
-      </a>
-    </Content>
-  </Container>
-);
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit">Register</Button>
+        </Form>
+
+        <a href="create">
+          <FiArrowLeft />
+          Back to Login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
