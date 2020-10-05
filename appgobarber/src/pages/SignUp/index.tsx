7ff7739e5636ce5a419960eae849e59831b91323
Guilherme Girardi <girardi.gui@icloud.com>
2020-10-05 08:03:43 +0100

    Edit navigation between Login and Create Account

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,9 +1,67 @@
 import React from "react";
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import { useNavigation } from "@react-navigation/native";
 
-import { Container } from "./styles";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import logoImg from "../../assets/logo.png";
+import { Container, Title, BackToSignIn, BackToSignInText } from "./styles";
 
-const SignUp: React.FC = () => {
-  return <Container />;
+const SingUp: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Create your account</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Name" />
+
+            <Input name="email" icon="mail" placeholder="Email" />
+
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button
+              onPress={() => {
+                console.log("deu");
+              }}
+            >
+              Enter
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff"></Icon>
+        <BackToSignInText>Back to Login</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
-export default SignUp;
+export default SingUp;
