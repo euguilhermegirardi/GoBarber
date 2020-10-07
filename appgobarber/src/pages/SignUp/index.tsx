24ecb9a0f60cdb029ebb31f0792ae36be169d812
Guilherme Girardi <girardi.gui@icloud.com>
2020-10-07 08:16:57 +0100

    Add axios to create account

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { Form } from "@unform/mobile";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
 
+import api from "../../services/api";
 import getValidationErrors from "../../utils/getValidationErrors";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
@@ -50,9 +51,11 @@ const SingUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post("/users", data);
+      await api.post("/users", data);
 
-      // history.push("/");
+      Alert.alert("Register complete!", "Now you can access the app!");
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
