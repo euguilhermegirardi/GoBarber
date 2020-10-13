946a1d1be9f86119069eef169cd6466a430ec0a2
Guilherme Girardi <girardi.gui@icloud.com>
2020-10-13 07:08:38 +0100

    Dependency Inversion Principle

--- back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,22 +1,38 @@
 // How it will manage the data from 'models'.
 // It will update, list, delete, etc...
 
-import { EntityRepository, Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from "../entities/Appointment";
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    // Create the repository
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
     // const findAppointment = this.appointments.find((appointment) =>
     //   isEqual(date, appointment.date)
     // );
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment>{
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
